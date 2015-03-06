9dd459e0f75762a4661bed1afe50251727fad788
Curtis Seaton <infin80design@gmail.com>
2015-03-05 19:07:43 -0800

    Add benchmark and working recursion solution

--- recursion-vs-iteration/recursion_matt.rb
+++ recursion-vs-iteration/recursion_matt.rb
@@ -1,3 +1,16 @@
+require "benchmark"
+
+def all_prime_factors_recursive(num, factors = [])
+
+  return nil if num == 1
+  return factors << num if is_prime?(num) # base case: breaks the recursion
+  # base case must be conditional
+
+  factors << lowest_factor(num)
+  num = num / lowest_factor(num)
+  all_prime_factors_recursive(num, factors) # the recursion
+end
+
 def is_prime?(num)
   factors = []
   range = (2..num-1).to_a
@@ -25,3 +38,10 @@ def is_prime?(num)
     end
   end
 end
+
+def lowest_factor(num)
+  range = (2..num-1).to_a
+  return range.find { |poss_factor| (num % poss_factor) == 0 } #finds the first possible factor
+end
+
+puts Benchmark.measure{all_prime_factors_recursive(123123123)}
