a82fa750ce7855af070facc85a4e5a90c22a8bc6
Curtis Seaton <infin80design@gmail.com>
2015-03-05 19:07:00 -0800

    Add working iteration method

--- recursion-vs-iteration/iteration.rb
+++ recursion-vs-iteration/iteration.rb
@@ -1,3 +1,5 @@
+require "benchmark"
+
 def all_prime_factors(num)
   prime_factors = []
   if prime?(num) == true
@@ -16,3 +18,38 @@ def all_prime_factors(num)
   end
   return prime_factors
 end
+
+def prime?(num)
+  factors = []
+  range = (2..num-1).to_a
+
+  if num == 1
+    return false
+
+  elsif num == 2
+    return true
+
+  elsif lowest_factor(num) == nil
+    return true
+
+  else
+    range.each do | poss_factor |
+      if num % poss_factor == 0
+        factors << poss_factor
+      end
+    end
+
+    if factors.length == 0
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def lowest_factor(num)
+  range = (2..num-1).to_a
+  return range.find { |poss_factor| (num % poss_factor) == 0 } #finds the first possible factor
+end
+
+puts Benchmark.measure{all_prime_factors(123123123)}
